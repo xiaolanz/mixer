4ba4e84b5cabe59ee107e4a47626793131162e81
xiaolanz <xiaolan@google.com>
2017-06-05 08:52:12 -0700

    clean up

--- adapter/serviceControl/reportMetrics.go
+++ adapter/serviceControl/reportMetrics.go
@@ -22,6 +22,7 @@ import (
 	"istio.io/mixer/adapter/statsd/config"
 	"istio.io/mixer/pkg/adapter"
 	"istio.io/mixer/pkg/pool"
+	"istio.io/mixer/bazel-mixer/external/io_bazel_rules_go_toolchain/src/cmd/go/testdata/testinternal3"
 )
 
 type (
@@ -58,7 +59,6 @@ func (b *builder) ValidateConfig(c adapter.Config) (ce *adapter.ConfigErrors) {
 func (*builder) NewMetricsAspect(env adapter.Env, cfg adapter.Config, metrics map[string]*adapter.MetricDefinition) (adapter.MetricsAspect, error) {
 	params := cfg.(*config.Params)
 
-	//TODO error handling
 	cs, err := createAPIClient(params.Address)
 
 	return &aspect{cs}, err
@@ -76,49 +76,7 @@ func (a *aspect) Record(values []adapter.Value) error {
 
 func (a *aspect) record(value adapter.Value) error {
 	//TODO mapping metrics to chemist proto
-	mname := value.Definition.Name
-	if t, found := a.templates[mname]; found {
-		buf := pool.GetBuffer()
-
-		// We don't check the error here because Execute should only fail when the template is invalid; since
-		// we check that the templates are parsable in ValidateConfig and further check that they can be executed
-		// with the metric's labels in NewMetricsAspect, this should never fail.
-		_ = t.Execute(buf, value.Labels)
-		mname = buf.String()
-		pool.PutBuffer(buf)
-	}
-
-	var result error
-	switch value.Definition.Kind {
-	case adapter.Gauge:
-		v, err := value.Int64()
-		if err != nil {
-			return fmt.Errorf("could not record gauge '%s': %v", mname, err)
-		}
-		result = a.client.Gauge(mname, v, a.rate)
-
-	case adapter.Counter:
-		v, err := value.Int64()
-		if err != nil {
-			return fmt.Errorf("could not record counter '%s': %v", mname, err)
-		}
-		result = a.client.Inc(mname, v, a.rate)
-	case adapter.Distribution:
-		// TODO: figure out how to program histograms via config.*
-		// updates
-		v, err := value.Duration()
-		if err == nil {
-			return a.client.TimingDuration(mname, v, a.rate)
-		}
-		// TODO: figure out support for non-duration distributions.
-		vint, err := value.Int64()
-		if err == nil {
-			return a.client.Inc(mname, vint, a.rate)
-		}
-		return fmt.Errorf("could not record distribution '%s': %v", mname, err)
-	}
-
-	return result
+	return nil
 }
 
 func (a *aspect) Close() error {
